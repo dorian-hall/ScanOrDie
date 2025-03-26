16f408a38b331b3a94f78935fa5b618e3efc12fc
Micheal Chen <che230732@spengergasse.at>
2025-03-26 19:43:46 +0100

    basicWwise

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,7 +14,38 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID PLAY_AIRPORTAMBIENCE = 4074573783U;
+        static const AkUniqueID PLAYDETECTOR = 4233473501U;
+        static const AkUniqueID PLAYEXPLOSIONSOUND = 1506756141U;
+        static const AkUniqueID PLAYFAILSOUND = 2004042764U;
+        static const AkUniqueID PLAYPICKUPBOMB = 2115165411U;
+        static const AkUniqueID PLAYPICKUPCLOTH = 2998817953U;
+        static const AkUniqueID PLAYPICKUPGLAS = 1492217638U;
+        static const AkUniqueID PLAYPICKUPMETAL = 1564899836U;
+        static const AkUniqueID PLAYPICKUPPLASTIC = 3518151353U;
+        static const AkUniqueID PLAYSUCCESSSOUND = 2096049309U;
+        static const AkUniqueID PLAYVICTORYSOUND = 2419550644U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace OBJECTTYPE
+        {
+            static const AkUniqueID GROUP = 951044540U;
+
+            namespace STATE
+            {
+                static const AkUniqueID CLOTH = 1367384683U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID PLASTIC = 660835419U;
+            } // namespace STATE
+        } // namespace OBJECTTYPE
+
+    } // namespace STATES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID BEEPSPEED = 1101615186U;
+    } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
